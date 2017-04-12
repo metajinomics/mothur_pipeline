f525d333cab4e4593a73efd7ee3993a40ed12bfc
Jin(Jinlyung) Choi <jinchoi721@gmail.com>
2017-04-12 12:44:01 -0500

    Update get_merge_file_command.py

--- get_merge_file_command.py
+++ get_merge_file_command.py
@@ -5,21 +5,6 @@
 
 import sys
 files = []
-group = []
-for file in open(sys.argv[1],'r'):
-    files.append(file.strip())
-    spl = file.strip().split('.')
-    group.append('.'.join(spl[:-2]))
-
-print "make.group(fasta=" + '-'.join(files)+ ", groups=" + '-'.join(group)+")"
-root@howe-iii:/mnt/rock# cat get_merge_file_command.py
-#!/usr/bin/python
-#this script generate mothur command for make.group using list file
-
-#usage: python get_make_group_command.py listfile > command
-
-import sys
-files = []
 
 for file in open(sys.argv[1],'r'):
     files.append(file.strip())
