ba750e4e5ff4fbdcf61c9dd8bcb110f1028175b5
Jin(Jinlyung) Choi <jinchoi721@gmail.com>
2018-04-18 13:05:04 -0500

    Update mothur_auto.sh

--- mothur_auto.sh
+++ mothur_auto.sh
@@ -6,25 +6,24 @@ for x in *-*;do mv $x ${x%-*}_${x##*-};done
 
 
 #this command make accnos file 
-for x in *.fna;do ../mothur/mothur "#list.seqs(fasta=$x)";done
+for x in *.fna;do /mnt/research/germs/softwares/mothur/mothur1.39.5/mothur "#list.seqs(fasta=$x)";done
 
 #this command read fna and accnos then make pick.fna
-for x in *.accnos;do ../mothur/mothur "#get.seqs(accnos=$x, fasta=${x%.accnos}.fna)";done
+for x in *.accnos;do /mnt/research/germs/softwares/mothur/mothur1.39.5/mothur "#get.seqs(accnos=$x, fasta=${x%.accnos}.fna)";done
 
 ls -1 *.pick.fna > list.pick
 
 python /mnt/home/choiji22/git/mothur_pipeline/get_make_group_command.py list.pick > group.batch
 
-../mothur/mothur group.batch
+/mnt/research/germs/softwares/mothur/mothur1.39.5/mothur group.batch
 
 
 python /mnt/home/choiji22/git/mothur_pipeline/get_merge_file_command.py list.pick > merge.batch
-../mothur/mothur merge.batch 
+/mnt/research/germs/softwares/mothur/mothur1.39.5/mothur merge.batch 
 
 
 
 #from here, run on mothur environment
-# if functional gene
-../mothur/mothur ../mothur_pipeline/step1.batch
-../mothur/mothur "#align.seqs(fasta=merged.unique.fasta, reference=/mnt/home/choiji22/dev/darteqm/$gen.fa)"
-../mothur/mothur ../mothur_pipeline/step2.batch
+/mnt/research/germs/softwares/mothur/mothur1.39.5/mothur /mnt/home/choiji22/git/mothur_pipeline/step1.batch
+/mnt/research/germs/softwares/mothur/mothur1.39.5/mothur "#align.seqs(fasta=merged.unique.fasta, reference=/mnt/home/choiji22/dev/darteqm/$gen.fa)"
+/mnt/research/germs/softwares/mothur/mothur1.39.5/mothur /mnt/home/choiji22/git/mothur_pipeline/step2.batch
